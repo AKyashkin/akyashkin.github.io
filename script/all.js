42367635943d0ba8ae97610028ffadd38d437b63
K Dima <dima.bfm@gmail.com>
2014-11-15 19:27:17 +0500

    fix

--- script/all.js
+++ script/all.js
@@ -1,15 +1,17 @@
 $(function() {
 
     var owlExternal = $(".global-carousel"),
-        owlInternal = $(".internal-slider");
-    var audio = document.querySelector('audio#firstly-player');
+        owlInternal = $(".internal-slider"),
+        owlFull = $(".full-width");
+
+     var audio = $('#firstly-player')[0];
     audioDesc = $('#desc-player')[0];
-    audioStart = function(){
+    /*audioStart = function(){
         setTimeout(function(){
          audio.play();
          audio.volume = 1;
-        },250)
-    }
+        },250);
+    }*/
     audioDescStart = function(){
         setTimeout(function(){
             audioDesc.currentTime = 0;
@@ -75,14 +77,14 @@ $(function() {
                     $('.img-overlay_wrap').animo( { animation: 'leftOpacity',duration:9, keep: true });
                     $('.logo').animo( { animation: 'fadeOut', duration: 6 , keep: true });
 
-                    audioStart();
+                    /*audioStart()*/
                 };
             animate();
 
         },
         afterAction:function(){
             if(this.currentItem === 1 && this.playDirection === "next"){
-                audioEnd();
+               /* audioEnd();*/
                 setTimeout(function(){
                     $('.img-overlay_wrap').removeClass('leftOpacity animated')
                     $(".img-overlay").removeClass('leftPullOverlay animated');
@@ -99,31 +101,94 @@ $(function() {
 
      });
 
-     owlInternal.owlCarousel({
-         singleItem: true,
-         navigation: false,
-         startDragging: function(){
-             if(this.currentItem === 0){
-                 if(this.newRelativeX < 0){
-                 }else{
-                     owlExternal.trigger("owl.prev");
-                     animate();
-                 }
-             }if(this.currentItem === this.maximumItem){
-                 if(this.newRelativeX < 0){
-                     owlExternal.trigger("owl.next")
-                 }else{
-                 }
-             }
-         },
-         afterInit: function(){
-             owlInternal.on("mousedown touchstart", function(event){
-                 event.stopPropagation();
-             })
+    owlInternal.owlCarousel({
+        singleItem: true,
+        navigation: false,
+        startDragging: function(){
+            if(this.currentItem === 0){
+                if(this.newRelativeX < 0){
+                }else{
+                    owlExternal.trigger("owl.prev");
+                    animate();
+                }
+            }if(this.currentItem === this.maximumItem){
+                if(this.newRelativeX < 0){
+                    owlExternal.trigger("owl.next")
+                }else{
+                }
+            }
+        },
+        afterInit: function(){
+            owlInternal.on("mousedown touchstart", function(event){
+                event.stopPropagation();
+            })
+            $('.open_full-width').on('mousedown touchstart', function(){
+                owlFull.fadeIn(500);
+                owlFull.append('<div class="close_full-width"></div>');
+                $('.close_full-width').on('mousedown touchstart', function(){
+                    owlFull.fadeOut(500);
+                })
+            })
+        },
+        afterMove: function(){
+            var owlFullData = owlFull.data('owlCarousel');
+            if(this.currentItem === 0){
+                owlFullData.goTo(0);
+            }if(this.currentItem === 1){
+                owlFullData.goTo(1);
+            }if(this.currentItem === 2){
+                owlFullData.goTo(2);
+            }if(this.currentItem === this.maximumItem){
+                owlFullData.goTo(3);
+            }
+        }
+    });
 
-         }
-     })
+    owlFull.owlCarousel({
+        slideSpeed : 450,
+        singleItem: true,
+        navigation: false,
+        startDragging: function(){
+            if(this.currentItem === 0){
+                if(this.newRelativeX < 0){
 
+                }else{
+                    owlExternal.trigger("owl.prev");
+                    animate();
+                    setTimeout(function(){
+                        owlFull.fadeOut(500)
+                    },250)
+                }
+            }if(this.currentItem === this.maximumItem){
+                if(this.newRelativeX < 0){
+                    owlExternal.trigger("owl.next")
+                    setTimeout(function(){
+                        owlFull.fadeOut(500)
+                    },250)
+                }else{
+
+                }
+            }
+
+        },
+        afterInit: function(){
+            owlFull.on("mousedown touchstart", function(event){
+                event.stopPropagation();
+            })
+        },
+        afterMove: function(){
+            var owlInternalData = owlInternal.data('owlCarousel');
+            if(this.currentItem === 0){
+                owlInternalData.goTo(0);
+            }if(this.currentItem === 1){
+                owlInternalData.goTo(1);
+            }if(this.currentItem === 2){
+                owlInternalData.goTo(2);
+            }if(this.currentItem === this.maximumItem){
+                owlInternalData.goTo(3);
+            }
+        }
+    });
 
 
     /*- Promo Carousel End -*/
@@ -163,7 +228,56 @@ $(function() {
     /*- Fancybox popups End -*/
 
 
+    /*- Double Click emulate on touch devices -*/
 
+    var isiOS = false;
+    var agent = navigator.userAgent.toLowerCase();
+    isiOS = true;
+
+    $.fn.doubletap = function(onDoubleTapCallback, onTapCallback, delay){
+        var eventName, action;
+        delay = delay == null? 500 : delay;
+        eventName = isiOS == true? 'touchend' : 'click';
+
+        $(this).bind(eventName, function(event){
+            var now = new Date().getTime();
+            var lastTouch = $(this).data('lastTouch') || now + 1 ;
+            var delta = now - lastTouch;
+            clearTimeout(action);
+            if(delta<500 && delta>0){
+                if(onDoubleTapCallback != null && typeof onDoubleTapCallback == 'function'){
+                    onDoubleTapCallback(event);
+                }
+            }else{
+                $(this).data('lastTouch', now);
+                action = setTimeout(function(evt){
+                    if(onTapCallback != null && typeof onTapCallback == 'function'){
+                        onTapCallback(evt);
+                    }
+                    clearTimeout(action);
+                }, delay, [event]);
+            }
+            $(this).data('lastTouch', now);
+        });
+    };
+    owlInternal.doubletap(
+            function(event){
+            owlFull.fadeIn(500);
+        },
+            function(event){
+        },
+        4000
+    );
+    owlFull.doubletap(
+        function(event){
+            owlFull.fadeOut(500);
+        },
+        function(event){
+        },
+        4000
+    );
+
+    /*- Double Click emulate on touch devices END -*/
 
 });
 
--- script/owl.carousel.js
+++ script/owl.carousel.js
@@ -187,7 +187,6 @@ if (typeof Object.create !== "function") {
             base.$userItems.wrapAll("<div class=\"owl-wrapper\">").wrap("<div class=\"owl-item\"></div>");
             base.$elem.find(".owl-wrapper").wrap("<div class=\"owl-wrapper-outer\">");
             base.wrapperOuter = base.$elem.find(".owl-wrapper-outer");
-            base.$elem.css("display", "block");
         },
 
         baseClass : function () {
